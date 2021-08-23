d7e94cfe6604987992fb91aac27f5ebf2ebfcbc5
User <user@MacBook-User.local>
2021-08-23 09:40:54 +0300

    add gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -2,46 +2,53 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/LitvinSO/go-todo-app"
 	"github.com/LitvinSO/go-todo-app/pkg/handler"
 	"github.com/LitvinSO/go-todo-app/pkg/repository"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
 	fmt.Println("get config - ", viper.GetString("db.host"))
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loaging env variables: %s", err.Error())
+	}
 
 	db, err := repository.NewPostgresDb(repository.Config{
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
 		Username: viper.GetString("db.username"),
-		Password: viper.GetString("db.password"),
+		Password: os.Getenv("DB_PASSWORD"),
 		DBname:   viper.GetString("db.dbname"),
 		SSLMode:  viper.GetString("db.sslmode"),
 		TimeZone: viper.GetString("db.timezone"),
 	})
 
 	if err != nil {
-		log.Fatalf("error initialize db: %s", err.Error())
+		logrus.Fatalf("error initialize db: %s", err.Error())
 	}
 
 	repos := repository.NewRepository(db)
 
 	if repos == nil {
-		log.Fatalf("error repos ")
+		logrus.Fatalf("error repos ")
 	}
 
 	handlers := new(handler.Handler)
 	srv := new(todo.Server)
 	err = srv.Run(viper.GetString("port"), handlers.InitRouts())
 	if err != nil {
-		log.Fatalf("error occured while running http Server: %s", err.Error())
+		logrus.Fatalf("error occured while running http Server: %s", err.Error())
 	}
 }
 
--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,9 +1,18 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/LitvinSO/go-todo-app"
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
 }
 
 func (h *Handler) signIn(c *gin.Context) {
